227f24b321ea0c0f7f0c31f9b85a7d3abf5c8719
mayetm93 <mayetm93@gmail.com>
2017-02-18 16:12:02 -0800

    first

---
+++ js/myscripts.js
@@ -0,0 +1,8 @@
+/*var thousandPoemAudio = document.getElementById('thousandPoemAudio');
+thousandPoemAudio.addEventListener('play', function(e) {
+    ga('send', 'event', '1000Poem', 'Click', 'Audio');
+});
+var specialMiraj = document.getElementById('specialMiraj');
+specialMiraj.addEventListener('play', function(e) {
+    ga('send', 'event', 'specialMiraj', 'Click', 'Audio');
+});*/
