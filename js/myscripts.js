a32f0b5a3d02bbc16dcd270c90c20118d652aae4
mayetm93 <mayetm93@gmail.com>
2017-02-20 17:35:08 -0800

    countdown

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,6 +3,7 @@ var delayMillis;
 $(document).ready(function() {
     $('.slider').slider({ indicators: false, interval: 2000 });
     firetoasts();
+    /*addEventCountDown();*/
 });
 
 var thousandPoemAudio = document.getElementById('thousandPoemAudio');
@@ -16,11 +17,17 @@ thetale.addEventListener('play', function(e) {
 
 firetoasts = function() {
     toasts = [
-        /*0*/"Salām, O Servant of The Beloved ﷺ!",
-        /*1*/"I’m here to help and give updates.",
-        /*2*/"Add me to your phone's homescreen.",
-        /*3*/"💚",
-        /*4*/""];
+        /*0*/
+        "Salām, O Servant of The Beloved ﷺ!",
+        /*1*/
+        "I’m here to help and give updates.",
+        /*2*/
+        "Add me to your phone's homescreen.",
+        /*3*/
+        "<span id='event-countdown'></span> 💚",
+        /*4*/
+        ""
+    ];
 
     for (var x = 0; x < 4; x++) {
         displaytoasts(x);
@@ -28,25 +35,22 @@ firetoasts = function() {
 }
 
 displaytoasts = function(toastCounter) {
-	delayMillis = 1500;
+    delayMillis = 1500;
     setTimeout(function() {
         Materialize.toast(toasts[toastCounter], 5500, 'rounded');
     }, delayMillis * (toastCounter + 1));
 }
 
 tabSelector = function(page) {
-	if (page == 'home') {
-		$('#home').trigger('click');
-		$('#dear').trigger('click');
-	}
-	else if (page == 'events') {
-		$('#info').trigger('click');
-		$('#youreloved').trigger('click');
-		$('#1000lines').trigger('click');
-	}
-	else if (page == 'love') {
-		$('#honesty').trigger('click');
-	}
+    /*if (page == 'home') {
+        $('#home').trigger('click');
+        $('#dear').trigger('click');
+    } else if (page == 'events') {
+        $('#info').trigger('click');
+        $('#youreloved').trigger('click');
+        $('#1000lines').trigger('click');
+    } else if (page == 'love') {
+        $('#honesty').trigger('click');
+        $('#honesty').addClass("active");
+    }*/
 }
-	
-	
