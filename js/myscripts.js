40756fbdb83d46af2d832a3008be61608cbedc08
mayetm93 <mayetm93@gmail.com>
2017-02-22 07:26:24 -0800

    sw

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,9 +1,15 @@
 var toasts;
-var delayMillis;
+var delayMillis = 1500;
 $(document).ready(function() {
     $('.slider').slider({ indicators: false, interval: 2000 });
+    $('ul.tabs').tabs({ swipeable: true });
     firetoasts();
-    /*addEventCountDown();*/
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function() { console.log('Service Worker Registered'); });
+    }
 });
 
 var thousandPoemAudio = document.getElementById('thousandPoemAudio');
@@ -20,37 +26,41 @@ firetoasts = function() {
         /*0*/
         "Salām, O Servant of The Beloved ﷺ!",
         /*1*/
-        "I’m here to help and give updates.",
+        "I’m getting some under-the-hood updates. So don't be worried if I'm broken for a few days. Sowe :<",
         /*2*/
-        "Add me to your phone's homescreen.",
+        "💚",
         /*3*/
-        "<span id='event-countdown'></span> 💚",
+        "Add me to your phone's homescreen.",
         /*4*/
         ""
     ];
 
-    for (var x = 0; x < 4; x++) {
+    for (var x = 0; x < 3; x++) {
         displaytoasts(x);
     }
 }
 
 displaytoasts = function(toastCounter) {
-    delayMillis = 1500;
     setTimeout(function() {
         Materialize.toast(toasts[toastCounter], 5500, 'rounded');
     }, delayMillis * (toastCounter + 1));
 }
 
 tabSelector = function(page) {
-    /*if (page == 'home') {
-        $('#home').trigger('click');
-        $('#dear').trigger('click');
+    if (page == 'home') {
+        $('ul.tabs').tabs('select_tab', 'home');
+        $('ul.tabs').tabs('select_tab', 'dear');
     } else if (page == 'events') {
-        $('#info').trigger('click');
+        $('ul.tabs').tabs('select_tab', 'info');
+        $('ul.tabs').tabs('select_tab', 'youreloved');
+        $('ul.tabs').tabs('select_tab', '1000lines');
+        /*$('#info').trigger('click');
         $('#youreloved').trigger('click');
-        $('#1000lines').trigger('click');
+        $('#1000lines').trigger('click');*/
     } else if (page == 'love') {
+        $('ul.tabs').tabs('select_tab', 'honesty');
+        /*$('ul.tabs').tabs('select_tab', 'tab_id');
         $('#honesty').trigger('click');
-        $('#honesty').addClass("active");
-    }*/
+        $('#honesty').addClass("active");*/
+    }
 }
